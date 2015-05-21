f2bd8c25b60f99fb9fb2c9164a3081dd19df52ba
Phillip Lopo <philliplopo@gmail.com>
2015-05-21 12:05:44 -0700

    adding simple repo gen script

---
+++ generate_repo.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.4
+
+from configparser import ConfigParser
+from urllib.parse import urlparse, urlunparse
+from urllib.request import urlopen
+import json
+from configparser import ConfigParser
+
+urls = open("repolist.txt", "r")
+
+repo_json = json.loads('{"packages": []}')
+
+for url in urls:
+    url = url.strip()
+    
+    pkg_json = None
+    try:
+        with urlopen(url + "/repo.json") as f:
+            pkg_json = f.read(524288).decode("utf-8")
+        pkg_json = json.loads(pkg_json)
+    except:
+        pkg_json = None
+
+    if not pkg_json:
+        continue
+
+    try:
+        pkg_json["name"]
+        pkg_json["description"]
+        pkg_json["version"]
+        pkg_json["repo"]
+    except:
+        continue
+
+    if "branch" not in pkg_json.keys():
+        pkg_json["branch"] = "master"
+
+    repo_json["packages"].append(pkg_json)
+
+
+repo_output = open("repo2.json", "wb")
+repo_output.write(json.dumps(repo_json, sort_keys=True, indent=4).encode("utf-8"))
+repo_output.write(b"\n")
+repo_output.close()
+
